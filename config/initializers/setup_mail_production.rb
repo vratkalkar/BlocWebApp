74337185db8847a5add1ee939429a87437d71191
Vivek <vratkalkar@live.com>
2014-03-03 16:48:05 -0500

    Updated setup_mail_production.rb

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:         'smtp.sendgrid.net',
-    port:             '587'
-    authentication:   :plain,
-    user_name:      ENV['app22330684@heroku.com'],
-    password:        ENV['ohlrhbpx'],
-    domain:          'heroku.com'
-    enable_starttls_auto: true
+    :address          =>'smtp.sendgrid.net',
+    :port             => '587',
+    :authentication   => :plain,
+    :user_name        => ENV['app22330684@heroku.com'],
+    :password         => ENV['ohlrhbpx'],
+    :domain           => 'heroku.com',
+    :enable_starttls_auto => true
 }
