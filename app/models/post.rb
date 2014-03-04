2c5fcfe2b5b61448b607e64c277b1143c30f0bff
Vivek <vratkalkar@live.com>
2014-03-04 17:49:21 -0500

    update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-  attr_accessible :body, :title , :topic 
+  attr_accessible :body, :title , :topics 
 
   default_scope order('created_at DESC')
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
+  validates :topics, presence: true
   validates :user, presence: true
  end
 
