bd4fd0b442c83538a1f05faa12a58a3d91104720
Vivek <vratkalkar@live.com>
2014-03-10 13:24:39 -0400

    Revert to f98a037037a27ead78d2ece710ed2856cafcb561

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   belongs_to :topic
-  attr_accessible :body, :title , :topic, :image
+  attr_accessible :body, :title , :topic
 
   default_scope order('created_at DESC')
-  mount_uploader :image, ImageUploader
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: { minimum: 20 }, presence: true
--- db/migrate/20140310145209_image.rb
+++
@@ -1,4 +0,0 @@
-class Image < ActiveRecord::Migration
-  def change
-  end
-end
