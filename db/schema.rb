25a223faaee2d373f3b8c60e3838284d5c75b118
Vivek <vratkalkar@live.com>
2014-03-10 11:25:58 -0400

    Update files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305152751) do
+ActiveRecord::Schema.define(version: 20140310145209) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
