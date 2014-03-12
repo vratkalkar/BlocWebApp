d01fcd6d516d27b2a06a6f1f989dc7ec0b200f50
Vivek <vratkalkar@live.com>
2014-03-12 10:56:05 -0400

    updated app views

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,10 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   attr_accessible :body, :title , :topic, :image
 
-  default_scope order('created_at DESC')
   mount_uploader :image, ImageUploader
+  
+  default_scope order('created_at DESC')
+  
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: { minimum: 20 }, presence: true
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -26,7 +26,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  #
+  
+  process :resize_to_fill => [300, 300]
+
   # def scale(width, height)
   #   # do something
   # end
@@ -35,9 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
-
-    process :resize_to_fill => [300, 300]
-
+     
     version :thumb do
       process :resize_to_fill => [50,50]
     end
