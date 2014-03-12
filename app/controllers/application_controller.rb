19ad409b12af7085af7ad3673a2653a8e9349c8e
Vivek <vratkalkar@live.com>
2014-03-12 10:52:01 -0400

    application controller update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:account_update) << :avatar
+      devise_parameter_sanitizer.for(:account_update) << :avatar << :name
     end
   end
 
